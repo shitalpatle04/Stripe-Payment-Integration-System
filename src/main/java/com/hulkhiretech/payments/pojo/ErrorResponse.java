a2038b7d0abb69a3f9067ca1de955e5fb92d796f
Shital Patle <shital.patle8702@gmail.com>
2026-07-05 13:25:00 +0530

    Remove old root Spring Boot project

--- src/main/java/com/hulkhiretech/payments/pojo/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.hulkhiretech.payments.pojo;
-
-import lombok.Data;
-
-@Data
-public class ErrorResponse {
-    private String errorCode;
-    private String errorMessage;
-}
--- src/main/java/com/hulkhiretech/payments/service/interfaces/PaymentsService.java
+++
@@ -1,9 +0,0 @@
-package com.hulkhiretech.payments.service.interfaces;
-
-import com.hulkhiretech.payments.pojo.CreatePaymentReq;
-import com.hulkhiretech.payments.pojo.PaymentResponse;
-
-public interface PaymentsService {
-
-	PaymentResponse createPayment(CreatePaymentReq createPaymentReq);
-}
